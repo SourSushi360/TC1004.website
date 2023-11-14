270a7116658deed68061307364c648dbb3d87039
SourSushi360 <jplopez365@hotmail.com>
2023-11-14 10:40:48 -0600

    created Home component

--- lector-placas/src/App.js
+++ lector-placas/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import {Header} from './components';
+import {Header, Home} from './components';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Home/>
     </div>
   );
 }
